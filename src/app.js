9e1f308a203d4d206fa20ee559daa1dc8939244a
KrishanGoel <krishan.goel@mindtickle.com>
2019-06-05 19:04:12 +0530

    reach router example

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import React from "react";
-import Todo from "./Todo";
+import { Router } from "@reach/router";
+/* import Todo from "./Todo";
 import Async from "./Async";
 import Timer from "./Timer";
+import { AnotherFunctionalComponent } from "./ClassFunctionalComponent"; // this is for multiple export file through object
+import ClassFunctionalComponent from "./ClassFunctionalComponent"; // this for export default file
+import JSX from "./JSX"; */
+import Home from "./Component/Home";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
+import Product from "./Component/Product";
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Todo />
+        {/*         <Todo />
         <Async />
         <Timer />
+        <ClassFunctionalComponent />
+        <AnotherFunctionalComponent />
+        <JSX /> */}
+        <Router>
+          <Home path="/" />
+          <About path="/about" />
+          <Contact path="/contact-us" />
+          <Product path="/product" />
+        </Router>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: "/"
+    publicPath: "/" // its redirect all server request to index page
   },
   devServer: {
     historyApiFallback: true
