cfab8e9fedb6f2c0b5093e6853f8f6e3b75ea149
KrishanGoel <krishan.goel@mindtickle.com>
2019-06-05 19:11:29 +0530

    deleted new files

--- src/app.js
+++ src/app.js
@@ -1,33 +1,7 @@
 import React from "react";
-import { Router } from "@reach/router";
-/* import Todo from "./Todo";
-import Async from "./Async";
-import Timer from "./Timer";
-import { AnotherFunctionalComponent } from "./ClassFunctionalComponent"; // this is for multiple export file through object
-import ClassFunctionalComponent from "./ClassFunctionalComponent"; // this for export default file
-import JSX from "./JSX"; */
-import Home from "./Component/Home";
-import About from "./Component/About";
-import Contact from "./Component/Contact";
-import Product from "./Component/Product";
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        {/*         <Todo />
-        <Async />
-        <Timer />
-        <ClassFunctionalComponent />
-        <AnotherFunctionalComponent />
-        <JSX /> */}
-        <Router>
-          <Home path="/" />
-          <About path="/about" />
-          <Contact path="/contact-us" />
-          <Product path="/product" />
-        </Router>
-      </div>
-    );
+    return <div>THis is new Component</div>;
   }
 }
 
