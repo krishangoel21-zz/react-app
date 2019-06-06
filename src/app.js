314855d180b40d84b87b65ca3b906a69d7994baa
KrishanGoel <krishan.goel@mindtickle.com>
2019-06-06 23:39:30 +0530

    add one example for componentdidmount

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import React from "react";
 /*import Home from "../Component/Home";*/
 
-import  ShouldUpdate  from "../Component/ShouldUpdate";
+/* import  ShouldUpdate  from "../Component/ShouldUpdate"; */
 
 class App extends React.Component {
+  componentDidMount() {
+    console.warn("Fetching data...");
+  }
+
   render() {
+    console.warn("rendering...");
     return (
       <div>
         THis is new Component
         {/* <Home data="king" /> */}
-        <ShouldUpdate />
+        {/*  <ShouldUpdate /> */}
       </div>
     );
   }
